a3bb01e96d1dfc539512e0094570849b5d38d1c5
CalebBolanos <bolanos.c@hotmail.com>
2018-05-13 22:07:14 -0500

    Estoy bien loco xdxdxd

--- PoliAsistenciaiOS/EstadisticasViewController.swift
+++ PoliAsistenciaiOS/EstadisticasViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  EstadisticasViewController.swift
 //  PoliAsistenciaiOS
 //
 //  Created by Caleb on 13/05/18.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class EstadisticasViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
--- PoliAsistenciaiOS/NotificacionesViewController.swift
+++ PoliAsistenciaiOS/NotificacionesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  Notificaciones.swift
 //  PoliAsistenciaiOS
 //
 //  Created by Caleb on 13/05/18.
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class NotificacionesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
