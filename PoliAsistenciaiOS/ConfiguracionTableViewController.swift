9a7603a5f09f235d11704cd7f7b3ae8f9b55c18b
CalebBolanos <bolanos.c@hotmail.com>
2018-05-15 14:38:35 -0500

    Configuracion

---
+++ PoliAsistenciaiOS/ConfiguracionTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConfiguracionTableViewController.swift
+//  PoliAsistenciaiOS
+//
+//  Created by Caleb on 15/05/18.
+//  Copyright © 2018 Disoft. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConfiguracionTableViewController : UITableViewController{
+    override func viewDidLoad() {
+        
+    }
+    @IBAction func atras(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
